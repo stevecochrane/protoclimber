fb6388632b727982aceeec619e8aa0c68fb0ceae
Steve Cochrane <steve@stevecochrane.com>
2016-06-23 22:27:19 -0500

    Added a new Tiled map with a gap in the wall.

--- source/Assets.hx
+++ source/Assets.hx
@@ -3,6 +3,6 @@ package;
 class Assets {
 
     //  Data
-    public static inline var TMX_PROTOCLIMBER_1:String = "data/protoclimber-1.tmx";
+    public static inline var TMX_PROTOCLIMBER_1:String = "data/protoclimber-2.tmx";
 
 }
