8a70b7c1751ce2d122e1d8a9bb26cd3bbceb0201
Steve Cochrane <steve@stevecochrane.com>
2016-10-14 22:54:19 -0500

    Added a new player sprite to be used for animations when climbing.

--- source/Assets.hx
+++ source/Assets.hx
@@ -5,4 +5,7 @@ class Assets {
     //  Data
     public static inline var TMX_PROTOCLIMBER_1:String = "data/protoclimber-2.tmx";
 
+    //  Images
+    public static inline var IMG_PLAYER:String = "images/player.png";
+
 }
