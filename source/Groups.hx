0608edc86f775aaf121090bc88f9c8fa987da21e
Steve Cochrane <steve@stevecochrane.com>
2016-11-25 17:27:23 -0600

    Added new global groups for AvalancheGenerator and AvalancheFallingIce.

--- source/Groups.hx
+++ source/Groups.hx
@@ -5,6 +5,8 @@ import flixel.group.FlxGroup;
 
 class Groups {
 
+    public static var avalancheFallingIce:FlxTypedGroup<AvalancheFallingIce> = new FlxTypedGroup<AvalancheFallingIce>(8);
+    public static var avalancheGenerators:FlxTypedGroup<AvalancheGenerator> = new FlxTypedGroup<AvalancheGenerator>();
     public static var climbZones:FlxTypedGroup<FlxSprite> = new FlxTypedGroup<FlxSprite>();
 
 }
