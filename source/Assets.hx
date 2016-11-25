d93bb9db4e1d6dc054dbc3c139f1372b2ca97d8a
Steve Cochrane <steve@stevecochrane.com>
2016-11-25 16:47:59 -0600

    Made a new protoclimber-3 tilemap with an AvalancheGenerator object in it.

--- source/Assets.hx
+++ source/Assets.hx
@@ -3,7 +3,7 @@ package;
 class Assets {
 
     //  Data
-    public static inline var TMX_PROTOCLIMBER_1:String = "data/protoclimber-2.tmx";
+    public static inline var TMX_PROTOCLIMBER_1:String = "data/protoclimber-3.tmx";
 
     //  Images
     public static inline var IMG_PLAYER:String = "images/player.png";
