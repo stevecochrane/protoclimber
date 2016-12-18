2561c639ce0bb46695cedfe39f7b4d440116fbc1
Steve Cochrane <steve@stevecochrane.com>
2016-12-17 21:04:15 -0600

    Now set to use the protoclimber-4 tilemap by default.

--- source/Assets.hx
+++ source/Assets.hx
@@ -3,7 +3,7 @@ package;
 class Assets {
 
     //  Data
-    public static inline var TMX_PROTOCLIMBER_1:String = "data/protoclimber-3.tmx";
+    public static inline var TMX_PROTOCLIMBER_1:String = "data/protoclimber-4.tmx";
 
     //  Images
     public static inline var IMG_PLAYER:String = "images/player.png";
