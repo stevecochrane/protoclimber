3b0402ab564ac31b3cfe10463161b78cce0fae4b
Steve Cochrane <steve@stevecochrane.com>
2016-12-21 22:16:36 -0600

    The player can now charge for a jump again, but now it isn't allowed only for the release.

--- source/Player.hx
+++ source/Player.hx
@@ -236,7 +236,7 @@ class Player extends FlxSprite {
         if (isCharging) {
             chargeTimer += elapsed;
 
-            if (chargeTimer >= 0.5 && stamina >= staminaChargedJumpCost) {
+            if (chargeTimer >= 0.5) {
                 isCharged = true;
                 FlxFlicker.flicker(this, 0, 0.04, true, false);
             }
@@ -625,10 +625,12 @@ class Player extends FlxSprite {
         if (isOnWall || isOnGround && isTouching(FlxObject.FLOOR)) {
             FlxG.log.add("Jump!");
 
-            if (isCharged && stamina >= staminaChargedJumpCost) {
-                velocity.y = baseGroundChargedJumpVelocity;
-                stamina -= staminaChargedJumpCost;
-                isOnWall = false;
+            if (isCharged) {
+                if (stamina >= staminaChargedJumpCost) {
+                    velocity.y = baseGroundChargedJumpVelocity;
+                    stamina -= staminaChargedJumpCost;
+                    isOnWall = false;
+                }
             } else if (stamina >= staminaJumpCost) {
                 velocity.y = baseGroundJumpVelocity;
                 stamina -= staminaJumpCost;
