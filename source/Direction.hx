9d2de9f1cd996d6ba622251c8b5c5f3acc7c30f4
Steve Cochrane <steve@stevecochrane.com>
2025-07-20 22:33:03 -0500

    Fix another build error by adding default,never to static Direction vars

--- source/Direction.hx
+++ source/Direction.hx
@@ -3,13 +3,13 @@ package;
 class Direction {
 
     //  Extends on the cardinal directions provided in FlxObject to include diagonals.
-    public static var UP:Int         = 0x0100;
-    public static var UP_RIGHT:Int   = 0x0110;
-    public static var RIGHT:Int      = 0x0010;
-    public static var DOWN_RIGHT:Int = 0x1010;
-    public static var DOWN:Int       = 0x1000;
-    public static var DOWN_LEFT:Int  = 0x1001;
-    public static var LEFT:Int       = 0x0001;
-    public static var UP_LEFT:Int    = 0x0101;
+    public static var UP         (default, never) : Int = 0x0100;
+    public static var UP_RIGHT   (default, never) : Int = 0x0110;
+    public static var RIGHT      (default, never) : Int = 0x0010;
+    public static var DOWN_RIGHT (default, never) : Int = 0x1010;
+    public static var DOWN       (default, never) : Int = 0x1000;
+    public static var DOWN_LEFT  (default, never) : Int = 0x1001;
+    public static var LEFT       (default, never) : Int = 0x0001;
+    public static var UP_LEFT    (default, never) : Int = 0x0101;
 
 }
